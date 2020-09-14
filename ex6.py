292e3ebd8cd046fc88942886113b9484c4fc6341
jaywomack <womackjw@hotmail.com>
2020-09-14 05:37:01 -0700

    Finished ex8

---
+++ ex6.py
@@ -0,0 +1,36 @@
+# set a variable to the value of 10
+types_of_people = 10
+# sets x to the value of a formatted string
+x = f"There are {types_of_people} types of people."
+# sets binary to the value of a string binary
+binary = "binary"
+# setsa variable do not to the string don't
+do_not = "don't"
+# sets the variable y to the value of a formatted string
+y = f"Those who know {binary} and those who {do_not}."
+
+# prints the x variable to the console
+print(x)
+# prints the y variable to the console
+print(y)
+
+# prints a formatted string to the console
+print(f"I said: {x}")
+# prints a formatted string to the console
+print(f"I also said: '{y}'")
+
+# creates a variable and sets it to false
+hilarious = False
+# sets a variable to a string that takes an input?
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+# prints the string and formats it with the value of hilarious var
+print(joke_evaluation.format(hilarious))
+
+# sets w to the value of a string
+w = "This is the left side of..."
+# sets e to the value of a string
+e = "a string with a right side."
+
+# concatenates w and e and prints it to the console
+print(w + e)
